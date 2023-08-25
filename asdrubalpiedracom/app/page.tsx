bfc881e3298f1b344793ef2aa47410a6b067c54f
Jason Piedra <42704934+Akisama213@users.noreply.github.com>
2023-08-24 18:01:42 -0600

    MASONRY DONE

--- asdrubalpiedracom/app/page.tsx
+++ asdrubalpiedracom/app/page.tsx
@@ -1,11 +1,12 @@
 import { fetchImageUrls } from 'google-photos-album-image-url-fetch';
 import * as fsPromise from 'fs/promises';
 import { readFileSync } from 'fs';
+import { ImageList } from '@mui/material';
 
 export default async function Home() {
   const list = readFileSync("./app/album.json", 'utf-8');
   const album = JSON.parse(list).map((pic : any) =>
-    <img style={{display:"block", width:"32.4vw", margin:"0.33vw"}} key={pic.url} src={pic.url}/>
+    <img style={{width:"100%", objectFit:"contain", height: "max-content", marginBottom:"10px"}} loading="lazy" src={pic.url}/>
   );
   const main = async () => {
     const re = await fetchImageUrls('https://photos.app.goo.gl/BSsiEqy2akQaKNQF6');
@@ -14,10 +15,10 @@ export default async function Home() {
   main().catch(er => console.error(er));
   
   return (
-    <main style={{overflowX:"hidden"}}>
-      <div style={{display:"flex", flexWrap:"wrap", width:"99.5vw"}}>
+    <main>
+      <ImageList variant="masonry" cols={3} gap={10}>
         {album}
-      </div>
+      </ImageList>
     </main>
   )
 }
